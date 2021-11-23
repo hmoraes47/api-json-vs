1c05d13a407d121826806ba43e2abba677d10e90
Henrique de Moraes <56733528+hmoraes47@users.noreply.github.com>
2021-11-22 23:00:39 -0300

    Hello
    Here i am

---
+++ StatusLogAPI/Arquivo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatusLogAPI
+{
+    internal class Arquivo
+    {
+
+        public class Rootobject
+        {
+            public Shipmentidentifier[] ShipmentIdentifiers { get; set; }
+            public string Latitude { get; set; }
+            public string Longitude { get; set; }
+            public DateTime UtcTimestamp { get; set; }
+            public string CustomerId { get; set; }
+        }
+
+        public class Shipmentidentifier
+        {
+            public string Type { get; set; }
+            public string Value { get; set; }
+        }
+    }
+}
---
+++ StatusLogAPI/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace StatusLogAPI
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine(value:"Hello World!");
+
+            await Get();
+        }
+
+        public static async Task Get()
+        {
+            var httpClient = new HttpClient();
+            var Username = "carriertesting@project44.com";
+            var Password = "IntegrationTesting";
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes($"{Username}:{Password}")));
+
+            var response = await httpClient.GetAsync(requestUri: "https://cloud.p-44.com/api/carriers/v2/tl/shipments/statusUpdates");
+
+            var data = await response.Content.ReadAsStringAsync();
+        }
+
+        public static async Task Post()
+        {
+            var httpClient = new HttpClient();
+            var request = new HttpRequestMessage();
+
+            var objeto = new Arquivo(); //Adicionar forma de transformar em objeto
+
+            var content = ToRequest(Arquivo); //Esperando receber um objeto
+
+            var response = await httpClient.PostAsync("https://cloud.p-44.com/api/carriers/v2/tl/shipments/statusUpdates", content);
+    }
+
+         public static StringContent ToRequest(object obj)
+        {
+            var json = JsonContent.SerializeObject(obj);
+            var data = new StringContent(json, Encoding.UTF8, mediaType: "application/json");
+
+            return data;
+        }
+    }
+}
